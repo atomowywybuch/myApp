db7c3467ab687208a97812e2b7eddfbacad0f9f3
atomowywybuch <atomowywybuch@gmail.com>
2017-02-16 19:03:06 +0100

    substance management - edit and delete functions
    Added functions in the controller to edit and delete substance

--- src/AppBundle/Controller/SubstanceController.php
+++ src/AppBundle/Controller/SubstanceController.php
@@ -13,26 +13,6 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class SubstanceController extends Controller
 {
-
-  /**
-  * @Route("/substance/new")
-  */
-
-  public function newAction()
-  {
-    $substances = new substances();
-    $substances->setName('Test_name'.rand(1,100));
-    $substances->setCas('Test_cas');
-    $substances->setWe('Test_we');
-    $substances->setVpressure('Test_vpressure');
-
-    $em = $this->getDoctrine()->getManager();
-    $em->persist($substances);
-    $em->flush();
-
-    return $this->render('substance/create.html.twig');
-  }
-
   /**
   * @Route("/substance/create", name="substance_create")
   */
@@ -111,8 +91,82 @@ class SubstanceController extends Controller
       'substance' => $substance
 
     ));
-
-
   }
 
+  /**
+  * @Route("/substance/edit/{id}")
+  */
+
+  public function editAction($id, Request $request)
+  {
+    $substance = $this->getDoctrine()
+      ->getRepository('AppBundle:substances')
+      ->find($id);
+
+      $substance->setName($substance->getName());
+      $substance->setCas($substance->getCas());
+      $substance->setWe($substance->getWe());
+      $substance->setVpressure($substance->getVpressure());
+
+      $form = $this->createFormBuilder($substance)
+        ->add('name')
+        ->add('cas', TextType::class, array('attr' =>array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+        ->add('we', TextType::class, array('attr' =>array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+        ->add('vpressure', TextType::class, array('attr' =>array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+        ->add('Save', SubmitType::class, array('label'=> 'Zapisz', 'attr' =>array('class' => 'btn-primary', 'style' => 'margin-bottom:15px')))
+        ->getForm();
+
+      $form->handleRequest($request);
+
+      if($form->isSubmitted() && $form->isValid()){
+        $name = $form['name']->getData();
+        $cas = $form['cas']->getData();
+        $we = $form['we']->getData();
+        $vpressure = $form['vpressure']->getData();
+
+        $em = $this->getDoctrine()->getManager();
+        $substance = $em->getRepository('AppBundle:substances')->find($id);
+
+        $substance->setName($name);
+        $substance->setCas($cas);
+        $substance->setWe($we);
+        $substance->setVpressure($vpressure);
+
+
+
+        $em->flush();
+
+        $this->addFlash(
+          'notice',
+          'Substancja zmieniona'
+        );
+
+        return $this->redirectToRoute('substance_list');
+      }
+
+    return $this->render('substance/edit.html.twig', array(
+      'substance' => $substance,
+      'form' => $form->createView()
+    ));
+  }
+
+  /**
+  * @Route("/substance/delete/{id}")
+  */
+
+  public function deleteAction($id)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $substance = $em->getRepository('AppBundle:substances')->find($id);
+
+    $em->remove($substance);
+    $em->flush();
+
+    $this->addFlash(
+      'notice',
+      'Substancja usunięta'
+    );
+
+    return $this->redirectToRoute('substance_list');
+  }
 }
