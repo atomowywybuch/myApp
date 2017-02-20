2f0bdaa9df0a01c63cfb6aee223097fab6c5dead
atomowywybuch <atomowywybuch@gmail.com>
2017-02-20 16:22:45 +0100

    adjustments in entity and controllers structure, kobize datalist implementation
    implemented Kobize data list

---
+++ src/AppBundle/Controller/admin/datalists/KobizeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller\admin\datalists;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class KobizeController extends Controller
+{
+    /**
+     * @Route("/admin/datalist/kobize", name="kobize")
+     */
+    public function indexAction()
+    {
+      //Gets all substances to the object substances
+      $kobize = $this->getDoctrine()
+        ->getRepository('AppBundle:datalist\kobize')
+        ->findAll();
+      //passess the substances object to the view
+      return $this->render('datalist/kobize/list.html.twig', array(
+        'kobize' => $kobize
+
+      ));
+    }
+}
---
+++ src/AppBundle/Entity/datalist/kobize.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity\datalist;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * kobize
+ *
+ * @ORM\Table(name="datalistkobize")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\datalist\kobizeRepository")
+ */
+class kobize
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="number", type="string", length=255)
+     */
+    private $number;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cas", type="string", length=255)
+     */
+    private $cas;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set number
+     *
+     * @param string $number
+     *
+     * @return kobize
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return string
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return kobize
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set cas
+     *
+     * @param string $cas
+     *
+     * @return kobize
+     */
+    public function setCas($cas)
+    {
+        $this->cas = $cas;
+
+        return $this;
+    }
+
+    /**
+     * Get cas
+     *
+     * @return string
+     */
+    public function getCas()
+    {
+        return $this->cas;
+    }
+}
